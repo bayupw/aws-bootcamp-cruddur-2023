2bc448612a397239cffb520c7137022bfd4a0072
Bayu Wibowo <bayupw@gmail.com>
2023-03-24 10:52:19 +1300

    replace hardcoded user_handle to retrieve from ActivityForm

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -137,7 +137,7 @@ def data_message_groups():
 
 @app.route("/api/messages/@<string:handle>", methods=['GET'])
 def data_messages(handle):
-  user_sender_handle = 'bayupwcruddur'
+  user_sender_handle = ["user_handle"] # '@user_handle'
   user_receiver_handle = request.args.get('user_reciever_handle')
 
   model = Messages.run(user_sender_handle=user_sender_handle, user_receiver_handle=user_receiver_handle)
@@ -150,7 +150,7 @@ def data_messages(handle):
 @app.route("/api/messages", methods=['POST','OPTIONS'])
 @cross_origin()
 def data_create_message():
-  user_sender_handle = 'bayupwcruddur'
+  user_sender_handle = ["user_handle"] # '@user_handle'
   user_receiver_handle = request.json['user_receiver_handle']
   message = request.json['message']
 
@@ -205,7 +205,7 @@ def data_search():
 @app.route("/api/activities", methods=['POST','OPTIONS'])
 @cross_origin()
 def data_activities():
-  user_handle  = 'bayupwcruddur'
+  user_handle = ["user_handle"] # '@user_handle'
   message = request.json['message']
   ttl = request.json['ttl']
   model = CreateActivity.run(message, user_handle, ttl)
@@ -223,7 +223,7 @@ def data_show_activity(activity_uuid):
 @app.route("/api/activities/<string:activity_uuid>/reply", methods=['POST','OPTIONS'])
 @cross_origin()
 def data_activities_reply(activity_uuid):
-  user_handle  = 'bayupwcruddur'
+  user_handle = ["user_handle"] # '@user_handle'
   message = request.json['message']
   model = CreateReply.run(message, user_handle, activity_uuid)
   if model['errors'] is not None:
