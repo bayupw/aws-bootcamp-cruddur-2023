51cf8a9e79989200ec5cb9174db7ec97b9e9e573
Bayu Wibowo <bayupw@gmail.com>
2023-03-23 19:14:09 +0000

    implemented create_activities and updated app.py

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -49,7 +49,7 @@ class Db:
 
     try:
       with self.pool.connection() as conn:
-        cur =  conn.cursor()
+        cur = conn.cursor()
         cur.execute(sql,params)
 
         if is_returning_id:
